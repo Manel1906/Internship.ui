f13b99838b193424cd930aefac28cb456148b7bb
DTK-18 <120298180+hodinhtuankiet@users.noreply.github.com>
2024-11-20 09:24:39 +0700

    Fix all manage specialty

--- WebContent/www/js/app/group/tpy/cat_test_img/ctrl/TpyTestImgLst.js
+++ WebContent/www/js/app/group/tpy/cat_test_img/ctrl/TpyTestImgLst.js
@@ -144,7 +144,7 @@ define([
 				do_lc_get_checked(dataCode)
 				do_get_list_ByAjax()
 			})
-			$("#inp_search").off("keydown").on("keydown", function(e){
+			$("#inp_search").off("input").on("input", function(e){
 				pr_SEARCH_KEY	= $(this).val();
 				do_gl_execute_debounce(do_lc_get_list);
 			});
