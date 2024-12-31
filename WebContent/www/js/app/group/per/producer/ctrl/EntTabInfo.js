07edc61df5571cfc61b897fcfa2c2b080adbaed4
hnv <hnv@hnv-tech.com>
2024-12-31 03:25:03 +0100

    change div_entity_content to div_entity_view

--- WebContent/www/js/app/group/per/producer/ctrl/EntTabInfo.js
+++ WebContent/www/js/app/group/per/producer/ctrl/EntTabInfo.js
@@ -84,7 +84,7 @@ define(['jquery','prjImageViewer/viewer'], function($,Viewer) {
 				const {path} = $(this).data();
 				let isImage = do_lc_check_image(path);
 				if(isImage){
-					const viewer = new Viewer(document.getElementById('div_entity_content'), {
+					const viewer = new Viewer(document.getElementById('div_entity_view'), {
 						filterImgClass: ['msg-body-forme', 'msg-body-other'],
 						hide: function () {
 							viewer.destroy();
